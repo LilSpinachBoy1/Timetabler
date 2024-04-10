63dd261bc0e57c5b381fb7ec4c8e637db3658809
Sam <sam@moocows.co.uk>
2024-04-10 09:41:13 +0100

    Added main.py and directories

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+WELCOME TO TIMETABLER!
+See README.md for more information on the project.
+This is main.py, the initial file to run for the project, it will house initial boot info and the main menu of the application.
+"""
+
+# Library imports
+import sys
+import time
+import pygame
+from pygame.locals import *
+
+print("Welcome to Timetabler!\nWatch this space...")
